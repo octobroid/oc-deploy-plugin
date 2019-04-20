ab9a77357139298931386be37167433551ebe4c6
Trias Nur Rahman <contact@triasrahman.com>
2019-04-20 21:48:31 +0700

    Update env and bitbucket provider

--- routes.php
+++ routes.php
@@ -5,8 +5,40 @@ use Symfony\Component\Process\Process;
 
 Route::post('deploy', function(Request $request) {
 
+    $provider = env('GIT_DEPLOY_PROVIDER'); // github, bitbucket, gitlab
+    $branch   = env('GIT_DEPLOY_BRANCH', 'master');
+    $key      = env('GIT_DEPLOY_KEY', '');
+    
+    $payload = $request->input();
+
+    switch ($provider) {
+        case 'github':
+            // @todo
+            break;
+        case 'bitbucket':
+            if (get('key') != $key) return 'Invalid key.';
+            
+            try {
+                if (isset($payload['push'])) {
+                    $lastChange = $payload['push']['changes'][count($payload['push']['changes']) - 1]['new'];
+                    $onBranch   = isset($lastChange['name']) && ! empty( $lastChange['name']) ? $lastChange['name'] : '';
+                    
+                    if ($branch != $onBranch) return 'Wrong branch.';
+                }
+            }
+            catch (Exception $e) {
+                return $e->getMessage();
+            }
+            break;
+        case 'gitlab':
+            // @todo
+            break;
+        default:
+            return 'Repository provider not defined.';
+    }
+        
     $root_path = base_path();
-    $process = new Process('cd ' . $root_path . '; ./deploy.sh');
+    $process = new Process('cd ' . $root_path . '; chmod +x deploy.sh; ./deploy.sh');
     $process->run(function ($type, $buffer) {
         echo $buffer;
     });
